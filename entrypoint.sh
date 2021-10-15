810f3c8da0c27a6b227e9edc234c2256c4fcfb51
mickey li <mickeyhli@outlook.com>
2021-10-15 14:55:24 +0100

    Updated action

--- entrypoint.sh
+++ entrypoint.sh
@@ -48,7 +48,7 @@ done
 echo "pre_release = $pre_release"
 echo "release = $release"
 echo ::set-output name=is_pre_release::$pre_release
-echo ::set-output name=is_release::false
+echo ::set-output name=is_release::$release
 
 # fetch tags
 git fetch --tags
